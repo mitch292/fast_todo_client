e5becd11275d10d75d9566c621fcb4f3d83e312e
Andrew Mitchell <andrewpmitchell7@gmail.com>
2020-12-23 22:41:08 -0500

    Use immer in zustand store

--- src/Tasks/useTaskStore.ts
+++ src/Tasks/useTaskStore.ts
@@ -1,36 +1,41 @@
-import create from "zustand";
+import create, { State, StateCreator } from "zustand";
+import produce, { Draft } from "immer";
 
 import { Task } from "./types";
 
-type State = {
-  tasks: Task[] | [];
+const immer = <T extends State>(
+  config: StateCreator<T, (fn: (draft: Draft<T>) => void) => void>
+): StateCreator<T> => (set, get, api) =>
+  config((fn) => set(produce(fn) as (state: T) => T), get, api);
+
+type TaskState = {
+  tasks: Task[];
   update: (t: Task) => void;
   remove: (t: Task) => void;
   create: (t: Task) => void;
   setTasks: (t: Task[]) => void;
 };
 
-export const useTaskStore = create<State>((set) => ({
-  tasks: [],
-  update: (t: Task) =>
-    set((state) => {
-      const newTasks = state.tasks.slice();
-      const index = newTasks.findIndex((task) => task.id === t.id);
-      newTasks[index] = t;
-      return { tasks: newTasks };
-    }),
-  remove: (t: Task) =>
-    set((state) => {
-      const newTasks = state.tasks.slice();
-      const index = newTasks.findIndex((task) => task.id === t.id);
-      newTasks.splice(index, 1);
-      return { tasks: newTasks };
-    }),
-  create: (t: Task) =>
-    set((state) => {
-      const newTasks = state.tasks.slice();
-      newTasks.unshift(t);
-      return { tasks: newTasks };
-    }),
-  setTasks: (t: Task[]) => set((state) => ({ tasks: t })),
-}));
+export const useTaskStore = create(
+  immer<TaskState>((set) => ({
+    tasks: [],
+    update: (t: Task) =>
+      set((state) => {
+        const index = state.tasks.findIndex((task) => task.id === t.id);
+        state.tasks[index] = t;
+      }),
+    remove: (t: Task) =>
+      set((state) => {
+        const index = state.tasks.findIndex((task) => task.id === t.id);
+        state.tasks.splice(index, 1);
+      }),
+    create: (t: Task) =>
+      set((state) => {
+        state.tasks.unshift(t);
+      }),
+    setTasks: (t: Task[]) =>
+      set((state) => {
+        state.tasks = t;
+      }),
+  }))
+);
